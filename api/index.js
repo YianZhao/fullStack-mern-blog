f1673267c7e637696402ad9b5d23976f83a9deef
Alex <alextanzhao22@gmail.com>
2024-08-10 23:21:07 -0700

    create a test api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 
 // 加载环境变量
 dotenv.config();
@@ -20,3 +21,7 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000!");
 });
+
+
+//测试
+app.use('/api/user',userRoutes)
