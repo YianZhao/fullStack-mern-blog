e165e23700d199e414000d84bb026e3dea994fa7
Alex <alextanzhao22@gmail.com>
2024-08-11 20:55:00 -0700

    create sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,37 +30,6 @@ export const signup = async(req,res,next)=>{
 
 
 
-// export const signup = async (req, res, next) => {
-//     const { username, email, password } = req.body;
-  
-//     if (
-//       !username ||
-//       !email ||
-//       !password ||
-//       username === '' ||
-//       email === '' ||
-//       password === ''
-//     ) {
-//       next(errorHandler(400, 'All fields are required'));
-//     }
-  
-//     const hashedPassword = bcryptjs.hashSync(password, 10);
-  
-//     const newUser = new User({
-//       username,
-//       email,
-//       password: hashedPassword,
-//     });
-  
-//     try {
-//       await newUser.save();
-//       res.json('Signup successful');
-//     } catch (error) {
-//       next(error);
-//     }
-//   };
-
-
 
 
 
