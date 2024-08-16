6f9c0d3c400e82582d7a5604826adb6d19364dc7
Alex <alextanzhao22@gmail.com>
2024-08-16 14:27:01 -0700

    complete update user profile functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react';
 
 const initialState = {
     currentUser:null,
@@ -23,12 +24,25 @@ const userSlice = createSlice({
         SignInFailure:(state,action)=>{
             state.loading = false;
             state.error = action.payload;
+        },
+        updateStart:(state)=>{
+            state.loading = true;
+            state.error = null;
+        },
+        updateSuccess:(state,action)=>{
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateFailure:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
         }
     }
 })
 
 
-export const {SignInStart,SignInSuccess,SignInFailure} = userSlice.actions
+export const {SignInStart,SignInSuccess,SignInFailure,updateStart,updateSuccess,updateFailure} = userSlice.actions
 
 export default userSlice.reducer
 
