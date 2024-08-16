9b5aa6aa9e3ee2013320722cdcbcf4290ffdb70d
Alex <alextanzhao22@gmail.com>
2024-08-16 16:10:17 -0700

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export const signin = async(req,res,next)=>{
 
         //弄一个密钥
         const token = jwt.sign(
-            {id : validUser._id},process.env.JWT_SECRET,
+            {id : validUser._id,isAdmin:validUser.isAdmin},process.env.JWT_SECRET,
         )
 
         //将密码在文档中分开
