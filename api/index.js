12b750e0f3c57ba217e544c8414e842f3a7f6aca
Alex <alextanzhao22@gmail.com>
2024-08-28 16:58:27 -0700

    all like functionality

--- api/index.js
+++ api/index.js
@@ -1,48 +1,43 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-import postRoutes from './routes/post.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 
-
-// 加载环境变量
 dotenv.config();
 
-// 连接到 MongoDB
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log("mongoDB is connected");
+    console.log('MongoDb is connected');
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
 
 const app = express();
+
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-
-
-// 启动服务器
 app.listen(3000, () => {
-  console.log("server is running on port 3000!");
+  console.log('Server is running on port 3000!');
 });
 
-
-//测试
-app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
-app.use('/api/post',postRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal Server Error'
-    res.status(statusCode).json({
-        succes:false,
-        statusCode,
-        message
-    })
-})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
